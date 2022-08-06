0e0142f4e36df882179901d4b30157b6592231f1
ThanhDoan <thanhdt2204@gmail.com>
2022-08-06 23:18:34 +0700

    Fix display page come back at not found page

--- src/views/error/NotFound.js
+++ src/views/error/NotFound.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from "react-router";
 
 class NotFound extends React.Component {
+
+    state = {
+        pageDestination: this.props.isLoggedIn ? 'home page' : 'login page'
+    }
 
     handleToHome = () => {
         this.props.history.push("/");
@@ -11,11 +16,15 @@ class NotFound extends React.Component {
         return (
             <div style={{ color: "white", textAlign: "center", marginTop: "260px", fontWeight: "bold" }}>
                 <div>Page not found</div>
-                <a href='# ' onClick={this.handleToHome}>Back to home page</a>
+                <a href='# ' onClick={this.handleToHome}>Back to {this.state.pageDestination}</a>
             </div>
         );
     }
 
 }
 
-export default withRouter(NotFound);
+const mapStateReduxToPropsOfApp = (state) => {
+    return { isLoggedIn: state.isLoggedIn }
+}
+
+export default connect(mapStateReduxToPropsOfApp, null)(withRouter(NotFound));
